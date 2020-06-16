e2365bb91fee26d49766c9d727e70d0500a15652
Anadi Nema <nema.anadi99@gmail.com>
2020-06-17 01:13:12 +0530

    Scores and Prefs

---
+++ app/src/main/java/com/assasin/varia/util/prefs.java
@@ -0,0 +1,11 @@
+package com.assasin.varia.util;
+
+import android.content.SharedPreferences;
+
+public class prefs {
+    private SharedPreferences preferences;
+
+    public prefs(SharedPreferences preferences) {
+        this.preferences = preferences;
+    }
+}
